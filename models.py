d9349e48a33bc0982ea349173575038ee74f75b6
Emiel Sebastiaan <emielvanderhoek@gmail.com>
2024-06-12 10:51:41 +0200

    Initiele test commit door Emiel PyCharm geinstalleerd Eerste wijziging classes: Header, Extrinsic

--- models.py
+++ models.py
@@ -2,12 +2,26 @@ from constants import VALIDATOR_COUNT
 
 
 class Header:
-    pass
+    def __init__(self):
+        self.parent_hash = '0x0000000000000000000000000000000000000000000000000000000000000000'
+        self.state_root_prior = '0x0000000000000000000000000000000000000000000000000000000000000000'
+        self.extrinsic_hash = '0x0000000000000000000000000000000000000000000000000000000000000000'
+        self.timeslot = 1
+        self.epoch = '0x00'
+        self.winning_tickets_marker = '0x00'
+        self.judgements_marker = '0x00'
+        self.author_key_idx = 1
+        self.vrf_signature = '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+        self.block_seal = '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
 
 
 class Extrinsic:
-    pass
-
+    def __init__(self):
+        self.tickets = {}
+        self.judgements = {}
+        self.preimages = {}
+        self.availability = {}
+        self.reports = {}
 
 class Block:
 
