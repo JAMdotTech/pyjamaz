e55872d47417fe31df788002844756150a9fb184
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2024-07-17 12:09:13 +0200

    Added Merkle trie testvectors

---
+++ pyjamaz/merkle.py
@@ -0,0 +1,16 @@
+
+
+def merkle(tree: dict) -> bytes:
+    """
+    Stub function for merkle tree
+
+    Parameters
+    ----------
+    tree: A nested dict of leaves to generate a merkle tree.
+
+    Returns
+    -------
+    Merklization in bytes
+    """
+    return bytes(32)
+
---
+++ test/test_merkle_trie.py
@@ -0,0 +1,18 @@
+import json
+import unittest
+
+from pyjamaz.merkle import merkle
+
+
+class TestMerkleTrie(unittest.TestCase):
+    def test_json_testvectors(self):
+        with open('./fixtures/trie.json') as f:
+            test_vector = json.load(f)
+
+        for item in test_vector:
+            output = merkle(item['input'])
+            self.assertEqual(item['output'], output.hex())
+
+
+if __name__ == '__main__':
+    unittest.main()
