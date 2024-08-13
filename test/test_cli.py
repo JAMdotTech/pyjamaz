01f77172a1ddce619b0760ba24c67266de38be89
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2024-08-13 14:44:57 +0200

    Use absolute paths

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,5 +1,7 @@
 import json
 import unittest
+from os import path
+
 from click.testing import CliRunner
 from pyjamaz.cli import main
 
@@ -7,10 +9,11 @@ from pyjamaz.cli import main
 class TestCLI(unittest.TestCase):
     def setUp(self):
         self.runner = CliRunner()
+        self.base_dir = path.join(path.dirname(path.abspath(__file__)), 'fixtures', 'cli')
 
     def test_import_blocks(self):
         result = self.runner.invoke(main, [
-            'import-blocks', "./fixtures/cli/initial-state.json", "./fixtures/cli/block_data"
+            'import-blocks', path.join(self.base_dir, "initial-state.json"), path.join(self.base_dir, "block_data")
         ])
         self.assertEqual(result.exit_code, 0)
         state = json.loads(result.output)
