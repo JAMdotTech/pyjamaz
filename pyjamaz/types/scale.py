c4de1beb7210cdcc484cb528b430f07c68187fea
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2024-08-14 16:53:49 +0200

    Serialization of variable size 29bit and 64bit Integers

---
+++ pyjamaz/types/scale.py
@@ -0,0 +1,147 @@
+import math
+
+from scalecodec.base import ScaleTypeDef, ScaleBytes
+from scalecodec.constants import TYPE_DECOMP_MAX_RECURSIVE
+from scalecodec.exceptions import ScaleEncodeException, ScaleDecodeException
+
+
+class VarInt29(ScaleTypeDef):
+    """
+    Implementation of variable size 29 bit Integer encoding as specified in GP-0.3.2-ref:274
+    """
+
+    def decode(self, data: ScaleBytes) -> int:
+
+        encoded = bytes(data.data)
+        prefix = encoded[0]
+        remainder = encoded[1:]
+        length = len(encoded)
+
+        if length == 1:  # Handles case for `value < 2**7`
+            return prefix
+
+        elif 1 < length < 4:  # Handles case for `2**7 <= value < 2**21`
+            value_part = prefix - (2 ** 8 - 2 ** (8 - length))
+            value = (value_part * 2 ** (8 * (length - 1))) + int.from_bytes(remainder, byteorder='little')
+        elif length == 4:  # Handles case for `2**21 <= value < 2**29`
+            value_part = prefix - (2 ** 8 - 2 ** 5)
+            value = (value_part * 2 ** 24) + int.from_bytes(remainder, byteorder='little')
+        else:
+            raise ScaleDecodeException("Unsupported length")
+
+        return value
+
+    def _encode(self, value: int) -> ScaleBytes:
+        """
+        Serializes a natural number x using a variable-length prefix with up to 4 bytes.
+
+        Parameters
+        ----------
+        value
+
+        Returns
+        -------
+        ScaleBytes
+        """
+        if value < 0:
+            raise ScaleEncodeException("Cannot encode negative value")
+
+        if value < 2**7:
+            return ScaleBytes(bytes([value]))
+
+        length = int(math.log2(value) // 7) + 1
+
+        if 2**7 <= value < 2**21:
+            prefix = (2 ** 8 - 2 ** (8 - length)) + (value // 2 ** (8 * (length - 1)))
+            remainder = (value % 2 ** (8 * (length - 1))).to_bytes(length - 1, byteorder='little')
+
+        elif 2**21 <= value < 2**29:
+            prefix = 2**8 - 2**5 + (value//2**24)
+            remainder = (value % 2**24).to_bytes(3, byteorder='little')
+
+        else:
+            raise ScaleEncodeException("Number too large for 29-bit variable-length encoding")
+
+        return ScaleBytes(bytes([prefix]) + remainder)
+
+    def serialize(self, value: int) -> int:
+        return value
+
+    def deserialize(self, value: int) -> int:
+        if type(value) is not int:
+            raise ValueError('Value must be an integer')
+        return value
+
+    def example_value(self, _recursion_level: int = 0, max_recursion: int = TYPE_DECOMP_MAX_RECURSIVE):
+        return 29
+
+
+class VarInt64(ScaleTypeDef):
+    """
+    Implementation of variable size 64 bit Integer encoding as specified in GP-0.3.2-ref:275
+    """
+
+    def decode(self, data: ScaleBytes) -> int:
+
+        encoded = bytes(data.data)
+        prefix = encoded[0]
+        remainder = encoded[1:]
+        length = len(encoded)
+
+        if length == 1:  # Handles case for `value < 2**7`
+            return prefix
+
+        elif 1 < length < 4:  # Handles case for `2**7 <= value < 2**21`
+            value_part = prefix - (2 ** 8 - 2 ** (8 - length))
+            value = (value_part * 2 ** (8 * (length - 1))) + int.from_bytes(remainder, byteorder='little')
+        elif length == 9:  # Handles case for `2**21 <= value < 2**64`
+            value_part = prefix - (2 ** 8 - 1)
+            value = int.from_bytes(remainder, byteorder='little')
+        else:
+            raise ScaleDecodeException("Unsupported length")
+
+        return value
+
+    def _encode(self, value: int) -> ScaleBytes:
+        """
+        Serializes a natural number x using a variable-length prefix with up to 4 bytes.
+
+        Parameters
+        ----------
+        value
+
+        Returns
+        -------
+
+        """
+        if value < 0:
+            raise ScaleEncodeException("Cannot encode negative value")
+
+        if value < 2**7:
+            return ScaleBytes(bytes([value]))
+
+        length = int(math.log2(value) // 7) + 1
+
+        if 2**7 <= value < 2**21:
+            prefix = (2 ** 8 - 2 ** (8 - length)) + (value // 2 ** (8 * (length - 1)))
+            remainder = (value % 2 ** (8 * (length - 1))).to_bytes(length - 1, byteorder='little')
+
+        elif 2**21 <= value < 2**64:
+            prefix = 2**8 - 1
+            remainder = value.to_bytes(8, byteorder='little')
+
+        else:
+            raise ScaleEncodeException("Number too large for 64-bit variable-length encoding")
+
+        return ScaleBytes(bytes([prefix]) + remainder)
+
+    def serialize(self, value: int) -> int:
+        return value
+
+    def deserialize(self, value: int) -> int:
+        if type(value) is not int:
+            raise ValueError('Value must be an integer')
+        return value
+
+    def example_value(self, _recursion_level: int = 0, max_recursion: int = TYPE_DECOMP_MAX_RECURSIVE):
+        return 64
---
+++ test/test_scale_varint.py
@@ -0,0 +1,68 @@
+import unittest
+
+from pyjamaz.types.scale import VarInt29, VarInt64
+from scalecodec.base import ScaleBytes
+from scalecodec.exceptions import ScaleEncodeException
+
+
+class TestVarInt29(unittest.TestCase):
+    def test_scale_encode(self):
+        var_int = VarInt29().new()
+
+        self.assertEqual('0x01', var_int.encode(1).to_hex())
+        self.assertEqual('0xc3e8', var_int.encode(1000).to_hex())
+        self.assertEqual('0xe4e093', var_int.encode(300000).to_hex())
+        self.assertEqual('0xffffff', var_int.encode(2**21-1).to_hex())
+        self.assertEqual('0xffffffff', var_int.encode(2**29 - 1).to_hex())
+
+    def test_encode_overflow(self):
+        var_int = VarInt29().new()
+
+        with self.assertRaises(ScaleEncodeException) as context:
+            var_int.encode(2**29)
+
+        with self.assertRaises(ScaleEncodeException) as context:
+            var_int.encode(-1)
+
+    def test_scale_decode(self):
+        var_int = VarInt29().new()
+
+        self.assertEqual(1, var_int.decode(ScaleBytes('0x01')))
+        self.assertEqual(1000, var_int.decode(ScaleBytes('0xc3e8')))
+        self.assertEqual(300000, var_int.decode(ScaleBytes('0xe4e093')))
+        self.assertEqual(2**29-1, var_int.decode(ScaleBytes('0xffffffff')))
+
+
+class TestVarInt64(unittest.TestCase):
+    def test_scale_encode(self):
+        var_int = VarInt64().new()
+
+        # self.assertEqual('0x01', var_int.encode(1).to_hex())
+        self.assertEqual('0xc3e8', var_int.encode(1000).to_hex())
+        self.assertEqual('0xe4e093', var_int.encode(300000).to_hex())
+        self.assertEqual('0xffffff', var_int.encode(2 ** 21 - 1).to_hex())
+        self.assertEqual('0xffffffff1f00000000', var_int.encode(2**29-1).to_hex())
+        self.assertEqual('0xffffffffffffffffff', var_int.encode(2**64 - 1).to_hex())
+
+    def test_encode_overflow(self):
+        var_int = VarInt64().new()
+
+        with self.assertRaises(ScaleEncodeException) as context:
+            var_int.encode(2**64)
+
+        with self.assertRaises(ScaleEncodeException) as context:
+            var_int.encode(-1)
+
+    def test_scale_decode(self):
+        var_int = VarInt64().new()
+
+        self.assertEqual(1, var_int.decode(ScaleBytes('0x01')))
+        self.assertEqual(1000, var_int.decode(ScaleBytes('0xc3e8')))
+        self.assertEqual(300000, var_int.decode(ScaleBytes('0xe4e093')))
+        self.assertEqual(2**21-1, var_int.decode(ScaleBytes('0xffffff')))
+        self.assertEqual(2**29-1, var_int.decode(ScaleBytes('0xffffffff1f00000000')))
+        self.assertEqual(2**64-1, var_int.decode(ScaleBytes('0xffffffffffffffffff')))
+
+
+if __name__ == '__main__':
+    unittest.main()
