7c4851c02117cb42bd3d585b68342cb9de49362e
matthijsb <matthijs@dynora.nl>
2024-09-27 12:43:40 +0200

    PVM MVP (#31)
    * - PVM test vectors WIP
    
    * - merged with main for new serialization methods
    
    * - cover all initial test vectors
    
    * - fix for jamcodec update
    
    * - fixes for code review
    
    * - Python numpy version compatibility test
    
    * - Python numpy version compatibility test
    
    * - Removed Python 3.8 & 3.9 support
    
    * - added typings
    
    * - fix for uint coercing difference between numpy 1.25 -> 2.1

---
+++ pyjamaz/pvm/types.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass, field
+from typing import List, Union, Type, T
+
+from jamcodec.base import JamBytes, JamCodecType
+from jamcodec.mixins import Serializable
+from jamcodec.types import VarInt64, Array, U8, BitArray, UnsignedInteger
+
+
+@dataclass
+class PVMProgram(Serializable):
+    jump_table_entry_count: int = field(metadata={'codec': VarInt64})
+    jump_table_entry_size: int = field(metadata={'codec': U8})
+    code_length: int = field(metadata={'codec': VarInt64})
+    jump_table: List[int] = field(metadata={'codec': Array(U8, 0)})
+    code: bytes = field(metadata={'codec': Array(U8, 0)})
+    opcode_bitmask: List[bool] = field(metadata={'codec': BitArray(0, strict_decoding=False)})  #TODO: remove strict_decoding=False
+
+    @classmethod
+    def from_jam_bytes(cls, scale_bytes: JamBytes) -> 'PVMProgram':
+        jump_table_entry_count = VarInt64.decode(scale_bytes)
+        jump_table_entry_size = U8.decode(scale_bytes)
+        code_length = VarInt64.decode(scale_bytes)
+
+        jump_table = Array(UnsignedInteger(jump_table_entry_size * 8), jump_table_entry_count).decode(scale_bytes)
+        code = Array(U8, code_length).decode(scale_bytes)
+        opcode_bitmask = BitArray(code_length, strict_decoding=False).decode(scale_bytes)   #TODO: remove strict_decoding=False
+
+        return cls(
+            jump_table_entry_count=jump_table_entry_count,
+            jump_table_entry_size=jump_table_entry_size,
+            code_length=code_length,
+            jump_table=jump_table,
+            code=code,
+            opcode_bitmask=opcode_bitmask,
+        )
+
+    def to_codec_type(self) -> JamCodecType:
+        codec_def = self.to_codec_def()
+        # Change definition according to data
+        codec_def.arguments['jump_table'] = Array(
+            UnsignedInteger(self.jump_table_entry_size * 8), self.jump_table_entry_count
+        )
+        codec_def.arguments['code'] = Array(U8, self.code_length)
+        codec_def.arguments['opcode_bitmask'] = BitArray(self.code_length)
+
+        scale_type = codec_def.new()
+        scale_type.deserialize(self)
+
+        return scale_type
+
+    @classmethod
+    def deserialize(cls: Type[T], data: Union[str, int, float, bool, dict, list]) -> T:
+        return cls.from_jam_bytes(JamBytes(bytes(data)))
+
+    def serialize(self) -> List[int]:
+        return [b for b in self.to_jam_bytes().to_bytes()]
