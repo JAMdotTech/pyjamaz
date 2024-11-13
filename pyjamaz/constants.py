a72a9b82f78f21c3c2d00c137bd19811449b09b8
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2024-11-13 16:02:19 +0100

    Modified WELL_KNOWN_STORAGE_KEYS

--- pyjamaz/constants.py
+++ pyjamaz/constants.py
@@ -1,28 +1,29 @@
 WELL_KNOWN_STORAGE_KEYS = {
     # Authorizer pool
-    1: int(1).to_bytes().ljust(32, b'\x00'),
+    1: bytes.fromhex('0100000000000000000000000000000000000000000000000000000000000000'),
     # Authorizer queue
-    2: int(2).to_bytes().ljust(32, b'\x00'),
+    2: bytes.fromhex('0200000000000000000000000000000000000000000000000000000000000000'),
     # Recent blocks
-    3: int(3).to_bytes().ljust(32, b'\x00'),
+    3: bytes.fromhex('0300000000000000000000000000000000000000000000000000000000000000'),
     # Safrole
-    4: int(4).to_bytes().ljust(32, b'\x00'),
+    4: bytes.fromhex('0400000000000000000000000000000000000000000000000000000000000000'),
     # Disputes
-    5: int(5).to_bytes().ljust(32, b'\x00'),
+    5: bytes.fromhex('0500000000000000000000000000000000000000000000000000000000000000'),
     # Entropy
-    6: int(6).to_bytes().ljust(32, b'\x00'),
+    6: bytes.fromhex('0600000000000000000000000000000000000000000000000000000000000000'),
     # Validator queue
-    7: int(7).to_bytes().ljust(32, b'\x00'),
+    7: bytes.fromhex('0700000000000000000000000000000000000000000000000000000000000000'),
     # Validator pool
-    8: int(8).to_bytes().ljust(32, b'\x00'),
+    8: bytes.fromhex('0800000000000000000000000000000000000000000000000000000000000000'),
     # Validator archive
-    9: int(9).to_bytes().ljust(32, b'\x00'),
+    9: bytes.fromhex('0900000000000000000000000000000000000000000000000000000000000000'),
     # Assurances
-    10: int(10).to_bytes().ljust(32, b'\x00'),
+    10: bytes.fromhex('0a00000000000000000000000000000000000000000000000000000000000000'),
     # Timeslot
-    11: int(11).to_bytes().ljust(32, b'\x00'),
+    11: bytes.fromhex('0b00000000000000000000000000000000000000000000000000000000000000'),
     # Privileged services
-    12: int(12).to_bytes().ljust(32, b'\x00'),
+    12: bytes.fromhex('0c00000000000000000000000000000000000000000000000000000000000000'),
     # Statistics
-    13: int(13).to_bytes().ljust(32, b'\x00'),
+    13: bytes.fromhex('0d00000000000000000000000000000000000000000000000000000000000000'),
 }
+
