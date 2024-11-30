041f3061bcf672d606a176c8f210462c81256a2f
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2024-11-30 17:24:52 +0800

    Enabled ticket submission

--- pyjamaz/app.py
+++ pyjamaz/app.py
@@ -608,24 +608,24 @@ class PyjamazApp:
 
     async def produce_block(self, timeslot: int) -> Block:
 
-        # if timeslot % EPOCH_TIMESLOTS > 0:
-        #     entropy = self.state.entropy.entropy[2]
-        #     TODO fix in other PR
-        #     if self.extrinsic.can_add_own_ticket(timeslot):
-        #
-        #         ring_public_keys = [v.bandersnatch for v in self.state.safrole.validators]
-        #
-        #         self.extrinsic.add_own_ticket(
-        #             ring_public_keys, entropy, self.config.keys.bandersnatch, self.get_author_index()
-        #         )
-        #
-        #         self.extrinsic.add_own_ticket(
-        #             ring_public_keys, entropy, self.config.keys.bandersnatch, self.get_author_index()
-        #         )
-        #
-        #         self.extrinsic.add_own_ticket(
-        #             ring_public_keys, entropy, self.config.keys.bandersnatch, self.get_author_index()
-        #         )
+        if timeslot % EPOCH_TIMESLOTS > 0:
+            entropy = self.state.entropy.entropy[2]
+
+            if self.extrinsic.can_add_own_ticket(timeslot):
+
+                ring_public_keys = [v.bandersnatch for v in self.state.safrole.validators]
+
+                self.extrinsic.add_own_ticket(
+                    ring_public_keys, entropy, self.config.keys.bandersnatch, self.get_author_index()
+                )
+
+                self.extrinsic.add_own_ticket(
+                    ring_public_keys, entropy, self.config.keys.bandersnatch, self.get_author_index()
+                )
+
+                self.extrinsic.add_own_ticket(
+                    ring_public_keys, entropy, self.config.keys.bandersnatch, self.get_author_index()
+                )
 
         extrinsic = Extrinsic(
             tickets=self.extrinsic.collect_tickets(),
