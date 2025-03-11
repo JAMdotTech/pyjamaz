3b011dd181c0c52ea75731f1a3d04629a1b43c57
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2025-03-11 22:14:24 +0100

    * updated tests

--- test/test_preimages.py
+++ test/test_preimages.py
@@ -12,8 +12,7 @@ from pyjamaz.state.base import AppContext
 from pyjamaz.state.components import Services
 from pyjamaz.storage import InMemoryStorage
 from pyjamaz.models.block import Header, BlockContext, Preimage
-from pyjamaz.models.state import TimeslotState, ServicesState, ServiceAccount, \
-    ServiceAccountMap, PreimageMap, StorageItemMap, PreimageAvailabilityMap
+from pyjamaz.models.state import TimeslotState, ServicesState, ServiceAccount
 
 
 def get_test_vector_files(file_filter: Optional[str] = None):
@@ -33,7 +32,7 @@ class TestPreimages(unittest.TestCase):
     def setUp(self):
         self.storage_engine = InMemoryStorage()
         self.block_context = BlockContext()
-        self.app_context = AppContext(transaction=None)
+        self.app_context = AppContext(transaction=self.storage_engine.transaction())
 
     @staticmethod
     def load_test_vector_data(test_vector_file):
@@ -67,6 +66,7 @@ class TestPreimages(unittest.TestCase):
         )
 
         pre_services.set_storage_engine(self.storage_engine)
+        pre_services.set_storage_transaction(self.app_context.transaction)
 
         # Store services and preimages in storage engine
         for s in test_vector["pre_state"]["accounts"]:
@@ -82,15 +82,17 @@ class TestPreimages(unittest.TestCase):
                 preimages={},
                 preimage_availability={}
             )
-            services.store_service_account(s["id"], service_account)
+            pre_services.store_service_account(s["id"], service_account)
             pre_services.retrieve_service_account(s["id"])
 
             for preimage in s["data"]["preimages"]:
-                services.store_service_preimage(Preimage(requester=s["id"], blob=bytes.fromhex(preimage["blob"][2:])))
+                pre_services.store_preimage(
+                    service_account_id=s["id"], preimage_blob=bytes.fromhex(preimage["blob"][2:])
+                )
                 pre_services.retrieve_preimage(s["id"], blake2b_256_hash(bytes.fromhex(preimage["blob"][2:])))
 
             for preimage in s["data"]["lookup_meta"]:
-                services.store_service_preimage_availability(
+                pre_services.store_preimage_availability(
                     service_account_id=s["id"],
                     preimage_hash=bytes.fromhex(preimage["key"]["hash"][2:]),
                     preimage_length=preimage["key"]["length"],
