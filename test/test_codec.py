f88666dceacd179535765730cb0fa9a5c29207a2
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2025-03-31 22:14:26 +0200

    Fix JDT stats codec unit test

--- test/test_codec.py
+++ test/test_codec.py
@@ -175,7 +175,7 @@ class TestCodec(unittest.TestCase):
         self.assertDictEqual(test_vector, value)
 
         with open(path.join(self.test_vector_jdt_dir, f'state_statistics.bin'), "rb") as f:
-           jam_data = f.read()
+            jam_data = f.read()
         self.assertEqual(jam_data.hex(), state.to_jam_bytes().to_bytes().hex())
 
     def test_jdt_state_timeslot(self):
