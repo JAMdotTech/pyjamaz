860e8a22555e0b58d971e9f1c901cc12e1ec2113
matthijsb <matthijs@dynora.nl>
2025-06-03 13:46:45 +0200

    - temp networking fix

--- pyjamaz/transport/protocol_jamnp_s.py
+++ pyjamaz/transport/protocol_jamnp_s.py
@@ -251,7 +251,7 @@ class ClientProtocol(JAMNPSProtocol):
 
                             case JAMNPS.MSG.UP0_BlockAnnouncement.value:
                                 logger.debug(f'ClientProtocol RECEIVED_BLOCK: {self._msg_len}')
-                                self.wrapper.pubsub.publish(PubSubSignal(topic=MESSAGE_TYPES.RECEIVED_BLOCK, data=self._msg_buffer[self._msg_offset:self._msg_len]))
+                                self.wrapper.broadcaster.send_stream.send_nowait(PubSubSignal(topic=MESSAGE_TYPES.RECEIVED_BLOCK, data=self._msg_buffer[self._msg_offset:self._msg_len]))
                                 # self.wrapper.broadcaster.send_stream.send_nowait({
                                 #     "message_type": MESSAGE_TYPES.RECEIVED_BLOCK,
                                 #     "data": self._msg_buffer[self._msg_offset:self._msg_len]
@@ -259,7 +259,7 @@ class ClientProtocol(JAMNPSProtocol):
 
                             case JAMNPS.MSG.CE128_BlockRequest.value:
                                 logger.debug(f'ClientProtocol RECEIVED REQUESTED BLOCKS: {self._msg_len}')
-                                self.wrapper.pubsub.publish(PubSubSignal(topic=MESSAGE_TYPES.REQUESTED_BLOCKS,
+                                self.wrapper.broadcaster.send_stream.send_nowait(PubSubSignal(topic=MESSAGE_TYPES.REQUESTED_BLOCKS,
                                                                          data=self._msg_buffer[self._msg_offset:self._msg_len]))
                                 # self.wrapper.broadcaster.send_stream.send_nowait({
                                 #     "message_type": MESSAGE_TYPES.REQUESTED_BLOCKS,
