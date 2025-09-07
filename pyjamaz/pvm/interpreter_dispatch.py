ce08638bd0b53cd74712e0dd8941a1efe84d30b4
matthijsb <matthijs@dynora.nl>
2025-09-07 11:28:02 +0200

    - opcode lookup / dispatch (WIP)

--- pyjamaz/pvm/interpreter_dispatch.py
+++ pyjamaz/pvm/interpreter_dispatch.py
@@ -1372,7 +1372,7 @@ def _op_store_imm_u64(vm):
 def _fetch_reg_imm_imm(vm):
     r_a = min(12, vm.code[vm.pc + 1] % 16)
     w_a = vm.reg[r_a]
-    l_x = int(min(4, vm.code[vm.pc + 1] % 8))
+    l_x = int(min(4, (vm.code[vm.pc + 1] // 16) % 8))
     v_x = pvm_X(read_uint(vm.mv_code, vm.pc + 2, l_x), l_x)
     l_y = int(min(4, max(0, vm.inst_arg_len[vm.inst_pos[vm.pc]] - l_x - 1)))
     v_y = pvm_X(read_uint(vm.mv_code, vm.pc + 2 + l_x, l_y), l_y)
@@ -1381,28 +1381,28 @@ def _fetch_reg_imm_imm(vm):
 
 def _op_store_imm_ind_u8(vm):
     r_a, w_a, v_x, v_y = _fetch_reg_imm_imm(vm)
-    addr = v_x + w_a
-    vm.mem_write(op_store_imm_ind_u8, addr, v_y % (1 << 8))
+    addr = u32(int(w_a) + int(v_x))
+    vm.mem_write(op_store_imm_ind_u8, addr, u8(v_y))
     vm.log and vm.log(reg1=r_a, imm1=v_x, imm2=v_y, context={"u'_(vx+wa)": vm._mem_read_int(addr, 1)})
 
 
 def _op_store_imm_ind_u16(vm):
     r_a, w_a, v_x, v_y = _fetch_reg_imm_imm(vm)
-    addr = v_x + w_a
-    vm.mem_write(op_store_imm_ind_u16, addr, v_y % (1 << 16))
+    addr = u32(int(w_a) + int(v_x))
+    vm.mem_write(op_store_imm_ind_u16, addr, u16(v_y))
     vm.log and vm.log(reg1=r_a, imm1=v_x, imm2=v_y, context={"u'_(vx+wa)": vm._mem_read_int(addr, 2)})
 
 
 def _op_store_imm_ind_u32(vm):
     r_a, w_a, v_x, v_y = _fetch_reg_imm_imm(vm)
-    addr = v_x + w_a
+    addr = u32(int(w_a) + int(v_x))
     vm.mem_write(op_store_imm_ind_u32, addr, u32(v_y))
     vm.log and vm.log(reg1=r_a, imm1=v_x, imm2=v_y, context={"u'_(vx+wa)": vm._mem_read_int(addr, 4)})
 
 
 def _op_store_imm_ind_u64(vm):
     r_a, w_a, v_x, v_y = _fetch_reg_imm_imm(vm)
-    addr = v_x + w_a
+    addr = u32(int(w_a) + int(v_x))
     vm.mem_write(op_store_imm_ind_u64, addr, v_y)
     vm.log and vm.log(reg1=r_a, imm1=v_x, imm2=v_y, context={"u'_(vx+wa)": vm._mem_read_int(addr, 8)})
 
