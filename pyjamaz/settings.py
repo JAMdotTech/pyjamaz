8af54dcb82975227775eaefbcffc93a863563bce
matthijsb <matthijs@dynora.nl>
2025-09-09 12:38:35 +0200

    V unittests fixen V forest V inlined versie naar eigen branch V pvm_gp -> numpy warnings surpressen V added CPYTHON and GP as pvm test targets

--- pyjamaz/settings.py
+++ pyjamaz/settings.py
@@ -1,3 +1,5 @@
+import os
+
 TEST_SUITE = 'tiny' # tiny or full
 GP_VERSION = '0.7.0'
 APP_VERSION = '0.1.8'
@@ -13,8 +15,9 @@ DEBUG_PROGRAM_OVERRIDE = {}
 
 
 PVM_DEBUGGER = None
+
 #PVM_INTERPRETER = "PVM_GP"
-PVM_INTERPRETER = "PVM_CPYTHON"
+PVM_INTERPRETER = os.getenv("PVM_INTERPRETER", "PVM_CPYTHON")
 PVM_MIN_HEAP_SIZE = 0
 PVM_MAX_HEAP_SIZE = 1_000_000*1000    #TODO: find out what it should be...
 
