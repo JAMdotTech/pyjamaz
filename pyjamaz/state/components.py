9dc1e30373eac0967940b9fe8ecdb8032af5765b
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2025-09-09 13:37:19 +0200

    Added simple exec time logger (#200)

--- pyjamaz/state/components.py
+++ pyjamaz/state/components.py
@@ -40,12 +40,13 @@ from pyjamaz.models.state import TimeslotState, EntropyState, ValidatorPoolState
     AccumulationHistoryState, ServiceAccount, AccumulationQueueState, AccumulationStateComponents, \
     AccumulationQueueWorkPackage, DeferredTransfer, ServiceActivityRecord
 from pyjamaz.transport.pubsub import PubSubSignal
-from pyjamaz.utils import reorder_list_outside_in, list_has_duplicates, format_hash
+from pyjamaz.utils import reorder_list_outside_in, list_has_duplicates, format_hash, log_execution_time
 
 
 class Timeslot(StateComponent):
     component_id = 11
 
+    @log_execution_time
     def state_transition(
             self,
             header: Header
@@ -80,6 +81,7 @@ class Timeslot(StateComponent):
 class Entropy(StateComponent):
     component_id = 6
 
+    @log_execution_time
     def state_transition(
             self,
             header: Header,
@@ -169,6 +171,7 @@ class ValidatorQueue(StateComponent):
 class ValidatorPool(StateComponent):
     component_id = 8
 
+    @log_execution_time
     def state_transition(
             self,
             header: Header,
@@ -212,6 +215,7 @@ class ValidatorPool(StateComponent):
 class ValidatorArchive(StateComponent):
     component_id = 9
 
+    @log_execution_time
     def state_transition(
             self,
             header: Header,
@@ -268,6 +272,7 @@ class Safrole(StateComponent):
         self.ring_data = ring_data
         self.post_state_safrole = None
 
+    @log_execution_time
     def create_ticket_body(self, ticket_data: TicketEnvelope, ring_context: RingContext, entropy: bytes) -> TicketBody:
         if ticket_data.attempt >= gp_const.TICKET_ENTRIES:
             raise StateTransitionError(SafroleErrorCode.bad_ticket_attempt)
@@ -285,6 +290,7 @@ class Safrole(StateComponent):
 
         return TicketBody(id=ring_vrf_output, attempt=ticket_data.attempt)
 
+    @log_execution_time
     def state_transition(
             self,
             header: Header,
@@ -505,6 +511,7 @@ class AuthorizerQueues(StateComponent):
 class AuthorizerPools(StateComponent):
     component_id = 1
 
+    @log_execution_time
     def state_transition(
             self,
             header: Header,
@@ -564,6 +571,7 @@ class AuthorizerPools(StateComponent):
 class RecentHistory(StateComponent):
     component_id = 3
 
+    @log_execution_time
     def state_transition_intermediate(
             self,
             header: Header,
@@ -594,6 +602,7 @@ class RecentHistory(StateComponent):
             intermediate_state=intermediate_state_recent_history
         )
 
+    @log_execution_time
     def state_transition(
             self,
             header: Header,
@@ -679,6 +688,7 @@ class RecentHistory(StateComponent):
 class Assurances(StateComponent):
     component_id = 10
 
+    @log_execution_time
     def state_transition_after_disputes(
             self,
             extrinsic_disputes: ExtrinsicDisputes,
@@ -745,7 +755,7 @@ class Assurances(StateComponent):
             if not self.has_valid_signature(assurance, validator):
                 raise StateTransitionError(AssurancesErrorCode.bad_signature)
 
-
+    @log_execution_time
     def state_transition_after_assurances(
             self,
             extrinsic_assurances: List[Assurance],
@@ -1099,6 +1109,7 @@ class Assurances(StateComponent):
 
         return False
 
+    @log_execution_time
     def state_transition_after_guarantees(
             self,
             extrinsic_guarantees: List[Guarantee],
@@ -1254,6 +1265,7 @@ class PrivilegedServices(StateComponent):
 class Disputes(StateComponent):
     component_id = 5
 
+    @log_execution_time
     def state_transition(
             self,
             extrinsic_disputes: ExtrinsicDisputes,
@@ -1585,6 +1597,7 @@ class Disputes(StateComponent):
 class Statistics(StateComponent):
     component_id = 13
 
+    @log_execution_time
     def state_transition(
             self,
             extrinsic_guarantees: List[Guarantee],
@@ -1788,6 +1801,7 @@ class Services(StateComponent):
             sorted_preimage(preimages[i]) <= sorted_preimage(preimages[i + 1]) for i in range(len(preimages) - 1)
         )
 
+    @log_execution_time
     def state_transition_after_preimages(
             self,
             extrinsic_preimages: List[Preimage],
@@ -1833,6 +1847,7 @@ class Services(StateComponent):
             post_state=intermediate_state_after_transfers
         )
 
+    @log_execution_time
     def state_transition_accumulation(
             self,
             accumulatable_work_reports: List[WorkReport],
@@ -1918,6 +1933,7 @@ class Services(StateComponent):
             accumulation_gas_utilized=output.accumulation_gas_utilized
         )
 
+    @log_execution_time
     def state_transition_transfers(
             self,
             intermediate_state_after_accumulation: ServicesState,
@@ -2075,6 +2091,7 @@ class Services(StateComponent):
 class AccumulationQueue(StateComponent):
     component_id = 14
 
+    @log_execution_time
     def state_transition(
             self,
             queued_work_reports: List[AccumulationQueueWorkPackage],
@@ -2133,6 +2150,7 @@ class AccumulationQueue(StateComponent):
 class AccumulationHistory(StateComponent):
     component_id = 15
 
+    @log_execution_time
     def state_transition(
             self,
             accumulatable_work_reports: List[WorkReport],
@@ -2175,6 +2193,7 @@ class AccumulationHistory(StateComponent):
 class RecentAccumulationLog(StateComponent):
     component_id = 16
 
+    @log_execution_time
     def state_transition(
             self
     ) -> None:
