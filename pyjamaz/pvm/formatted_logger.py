6f79694e566718ca71b1c0a8b5adbe3799de7702
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2025-09-12 13:37:25 +0200

    Simple parent-based forking

--- pyjamaz/pvm/formatted_logger.py
+++ pyjamaz/pvm/formatted_logger.py
@@ -1,4 +1,6 @@
 import logging
+
+from pyjamaz import settings
 from pyjamaz.pvm.constants import OpcodeNames
 from pyjamaz.pvm.debug_logger import PVMDebugLog
 
@@ -57,20 +59,21 @@ class PVMFormattedLog(PVMDebugLog):
         logging.log(log_lvl, f'{prefix_str}{spacing}{message}')
 
     def __call__(self, reg1=None, reg2=None, reg3=None, imm1=None, imm2=None, off1=None, off2=None, context=None):
-        regs = self._pvm.get_registers()
+        if settings.DEBUG:
+            regs = self._pvm.get_registers()
 
-        opn = OpcodeNames[self._pvm.opcode]
+            opn = OpcodeNames[self._pvm.opcode]
 
-        inst_str = (
-            f"{self._pvm.inst_nr}: "
-            f"PC {self._pvm.pc} "
-            f"{opn} ({self._pvm.opcode})"
-        )
-        spacing = " " * (51 - len(str(inst_str)))
-        logging.debug(
-            f"{inst_str}"
-            f"{spacing}"
-            f"g={self._pvm.gas} "
-            #f"pvmHash={format_hash(self.hash())} "
-            f"reg={str(regs)}"
-        )
+            inst_str = (
+                f"{self._pvm.inst_nr}: "
+                f"PC {self._pvm.pc} "
+                f"{opn} ({self._pvm.opcode})"
+            )
+            spacing = " " * (51 - len(str(inst_str)))
+            logging.debug(
+                f"{inst_str}"
+                f"{spacing}"
+                f"g={self._pvm.gas} "
+                #f"pvmHash={format_hash(self.hash())} "
+                f"reg={str(regs)}"
+            )
