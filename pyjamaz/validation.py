adb9c6dc3141a4a347fb2ca6b8f4b5d83d1143c9
Arjan Zijderveld <5286904+arjanz@users.noreply.github.com>
2025-12-23 12:43:45 +0100

    Missing condition for tickets marker (#236)

--- pyjamaz/validation.py
+++ pyjamaz/validation.py
@@ -48,20 +48,14 @@ class BlockValidation:
     ):
 
         # Check marker data
-        if header.tickets_marker is not None:
-            if header.slot_phase_index < TICKET_SUBMISSION_END_SLOT:
-                # ticket marker *cannot* be set before TICKET_SUBMISSION_END_SLOT
-                raise BlockValidationError(BlockValidationErrorCode.bad_ticket_marker_data)
-            elif header.tickets_marker != safrole_output.tickets_mark:
-                # If set, ticket marker *must* match STF aux output
-                raise BlockValidationError(BlockValidationErrorCode.bad_ticket_marker_data)
+        if header.tickets_marker != safrole_output.tickets_mark:
+            raise BlockValidationError(BlockValidationErrorCode.bad_ticket_marker_data)
 
         if header.epoch_marker != safrole_output.epoch_mark:
             raise BlockValidationError(BlockValidationErrorCode.bad_epoch_marker_data)
 
         if header.offenders_marker != disputes_output.offenders_mark:
             raise BlockValidationError(BlockValidationErrorCode.bad_offender_marker_data)
-
 
         # Validate seal
         entropy = post_entropy.entropy[3]
